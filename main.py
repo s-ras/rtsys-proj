0de22d7350a964346a8e5e5f6b3662de374856e3
s-ras <rasolisepehr@gmail.com>
2025-06-01 20:16:30 +0330

    added more example tasks to each processor, fixed tasks for deadline monotonic not being admissible

--- main.py
+++ main.py
@@ -9,24 +9,28 @@ def main():
         Task(name="T1", period=20, deadline=7, execution_time=3),
         Task(name="T2", period=5, deadline=4, execution_time=2),
         Task(name="T3", period=10, deadline=8, execution_time=1),
+        Task(name="T4", period=25, deadline=12, execution_time=3),
     )
     p2 = Processor("Deadline Monotonic", 4, DM(), 100)
     p2.add_tasks(
-        Task(name="T1", period=20, deadline=7, execution_time=3),
-        Task(name="T2", period=5, deadline=4, execution_time=2),
-        Task(name="T3", period=10, deadline=9, execution_time=2),
+        Task(name="T1", period=20, deadline=10, execution_time=3),
+        Task(name="T2", period=15, deadline=7, execution_time=2),
+        Task(name="T3", period=10, deadline=9, execution_time=1),
+        Task(name="T4", period=25, deadline=12, execution_time=3),
     )
     p3 = Processor("Earliest Deadline First", 4, EDF(), 100)
     p3.add_tasks(
         Task(name="T1", period=20, deadline=7, execution_time=3),
         Task(name="T2", period=5, deadline=4, execution_time=2),
         Task(name="T3", period=10, deadline=8, execution_time=1),
+        Task(name="T4", period=25, deadline=12, execution_time=3),
     )
     p4 = Processor("Least Laxity First", 4, LLF(), 100)
     p4.add_tasks(
         Task(name="T1", period=20, deadline=7, execution_time=3),
         Task(name="T2", period=5, deadline=4, execution_time=2),
         Task(name="T3", period=10, deadline=8, execution_time=1),
+        Task(name="T4", period=25, deadline=12, execution_time=3),
     )
     p1.start()
     p2.start()
